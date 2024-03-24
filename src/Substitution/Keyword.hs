2f7f8ecaf3d4ebc8117318dcbeaf2d38c43d15d2
adrian052 <a_drian1@hotmail.es>
2024-03-24 11:12:46 -0600

    Added keyword cypher

---
+++ src/Substitution/Keyword.hs
@@ -0,0 +1,33 @@
+module Substitution.Keyword (
+    encryptKeyword,
+    decryptKeyword
+) where
+
+import Data.Maybe(fromJust)
+import Data.List (elemIndex)
+
+import Data.Char
+
+prepareKeyword :: String -> String
+prepareKeyword keyword = removeDuplicates (map toUpper keyword)
+
+removeDuplicates :: String -> String
+removeDuplicates [] = []
+removeDuplicates (x:xs)
+    | x `elem` xs = removeDuplicates xs
+    | otherwise = x : removeDuplicates xs
+
+createEncryptionAlphabet :: String -> String
+createEncryptionAlphabet keyword = prepareKeyword keyword ++ filter (`notElem` prepareKeyword keyword) ['A'..'Z']
+
+encryptKeyword :: String -> String -> String
+encryptKeyword keyword plaintext =
+    let alphabet = createEncryptionAlphabet keyword
+        encryptChar ch = if isLetter ch then alphabet !! (ord (toUpper ch) - ord 'A') else ch
+    in map encryptChar plaintext
+
+decryptKeyword :: String -> String -> String
+decryptKeyword keyword ciphertext =
+    let alphabet = createEncryptionAlphabet keyword
+        decryptChar ch = if isLetter ch then ['A'..'Z'] !! fromJust (elemIndex ch alphabet) else ch
+    in map decryptChar ciphertext
