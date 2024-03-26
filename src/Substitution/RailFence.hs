f736e2a2a73657c55da60a01801ac00ae404e0c8
adrian052 <a_drian1@hotmail.es>
2024-03-26 09:01:04 -0600

    Added Rail Fence decryption

--- src/Substitution/RailFence.hs
+++ src/Substitution/RailFence.hs
@@ -22,3 +22,26 @@ getChars tuples = map (\(char, _) -> char) tuples
 
 encryptRailFence :: String -> Int -> String
 encryptRailFence str depth = getChars $ sortBy compareByFence (labelRail str True 1 1 depth)
+
+
+labelRail2 :: String -> Bool -> Int -> Int -> Int -> [(Int,Int)]
+labelRail2 [] _ _ _ _               = []
+labelRail2 (_:xs) down row col depth  = [(row, col)] ++ labelRail2 xs newDown newRow (col+1) depth
+                                    where newDown = if (down && depth==row) || (not(down) && row==1)
+                                                    then not(down)
+                                                    else down 
+                                          newRow = if(newDown) then row+1 else row-1 
+
+compareByFirst :: (Int,Int) -> (Int,Int) -> Ordering
+compareByFirst (a, b) (c, d)
+    | a /= c   = compare a c
+    | otherwise = compare b d
+
+compareByFence2 :: (Char, (Int,Int)) -> (Char, (Int,Int)) -> Ordering
+compareByFence2 (_, (a, b)) (_, (c, d))
+    | b /= d   = compare b d
+    | otherwise = compare a c
+
+decryptRailFence :: String -> Int -> String 
+decryptRailFence str depth = 
+    getChars (sortBy compareByFence2 (zip str (sortBy compareByFirst (labelRail2 str True 1 1 depth))))
