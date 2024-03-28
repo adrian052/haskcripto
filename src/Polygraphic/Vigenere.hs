d83458763a1c2eeff3f82b93260eb0bfe1bf737c
adrian052 <a_drian1@hotmail.es>
2024-03-28 08:50:19 -0600

    Added vigenere decryption

--- src/Polygraphic/Vigenere.hs
+++ src/Polygraphic/Vigenere.hs
@@ -1,5 +1,6 @@
 module Substitution.RailFence (
     encryptVigenere
+    decryptVigenere
 ) where
 
 
@@ -8,6 +9,10 @@ import Data.Char
 encryptChar :: (Char, Char) -> Char
 encryptChar (char, key) = shift numKey char
                         where numKey = ord key - ord 'A'
+
+decryptChar :: (Char, Char) -> Char
+decryptChar (char, key) = shift numKey char
+                        where numKey = -(ord key - ord 'A')
 
 shift :: Int -> Char -> Char
 shift shift char = chr $ (ord 'A' + (ord char - ord 'A' + shift) `mod` 26)
@@ -21,8 +26,16 @@ repeatAndConcat str = str ++ repeatAndConcat str
 encryptVigenere :: String -> String -> String
 encryptVigenere word keyword = if ((all isAlpha word) && (all isAlpha keyword))
                                 then map encryptChar zipped  
-                                else error "Cannot encrypt use alpha chars"
+                                else error "Cannot encrypt, please use alpha chars"
                                 where upperWord = toUpperWord word
                                       upperKeyword = toUpperWord keyword
                                       zipped = zip upperWord (repeatAndConcat upperKeyword)
 
+
+decryptVigenere :: String -> String -> String
+decryptVigenere word keyword = if ((all isAlpha word) && (all isAlpha keyword))
+                                then map decryptChar zipped  
+                                else error "Cannot encrypt use alpha chars"
+                                where upperWord = toUpperWord word
+                                      upperKeyword = toUpperWord keyword
+                                      zipped = zip upperWord (repeatAndConcat upperKeyword)
