8bd46ccd7926222d558fb9a61c5aecc755fbf884
adrian052 <a_drian1@hotmail.es>
2024-03-30 08:57:16 -0600

    Added function to createDiagram

--- src/Polygraphic/Playfair.hs
+++ src/Polygraphic/Playfair.hs
@@ -1,5 +1,6 @@
 
 import Data.Char
+import qualified Data.Set as Set
 
 formatKeyword :: String -> String
 formatKeyword str = if ((all isAlpha str) && (all isAlpha str))
@@ -10,3 +11,15 @@ formatKeyword str = if ((all isAlpha str) && (all isAlpha str))
                         removeDuplicated (x:xs) = x : removeDuplicated (filter (/= x) xs)
                         convertJI str = map (\x -> if x == 'J' then 'I' else x) str
                         toUpperWord str = map toUpper str
+
+createDiagramList :: String -> String
+createDiagramList keyword = keyword ++ abcWithoutKeys (Set.fromList keyword) 
+                            where
+                            abcWithoutKeys keys = filter (\x -> x/='J' && Set.notMember x keys) ['A'..'Z']
+
+createDiagram :: String -> [String] 
+createDiagram unformatedKey = toMatrix(createDiagramList $ formatKeyword unformatedKey)
+                              where
+                              chunk _ [] = []
+                              chunk n xs = take n xs : chunk n (drop n xs)
+                              toMatrix = chunk 5
