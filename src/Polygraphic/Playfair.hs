b7f16239eb4c1a7435c3908b16d2a29dc9e0f6fb
adrian052 <a_drian1@hotmail.es>
2024-03-31 15:42:16 -0600

    Added diagrams to playfair

--- src/Polygraphic/Playfair.hs
+++ src/Polygraphic/Playfair.hs
@@ -2,6 +2,37 @@
 import Data.Char
 import qualified Data.Set as Set
 
+--Algorithm functions
+createList :: String -> String
+createList keyword = keyword ++ abcWithoutKeys (Set.fromList keyword) 
+                            where
+                            abcWithoutKeys keys = filter (\x -> x/='J' && Set.notMember x keys) ['A'..'Z']
+
+createMatrix :: String -> [String] 
+createMatrix unformatedKey = toMatrix(createList $ formatKeyword unformatedKey)
+                              where
+                              chunk _ [] = []
+                              chunk n xs = take n xs : chunk n (drop n xs)
+                              toMatrix = chunk 5
+
+getDiagrams :: String -> [(Char,Char)]
+getDiagrams text = buildDiagrams $ formatText text
+
+buildDiagrams :: String -> [(Char,Char)]
+buildDiagrams []        = []
+buildDiagrams [x]       = [(x,'X')]
+buildDiagrams (x:y:z)   
+                    | x == y    = [(x,'X')] ++ buildDiagrams (y:z)
+                    | otherwise = [(x,y)]  ++ buildDiagrams z
+
+
+---Helper functions
+convertJI :: String -> String
+convertJI str = map (\x -> if x == 'J' then 'I' else x) str
+
+toUpperWord :: String -> String
+toUpperWord str = map toUpper str
+
 formatKeyword :: String -> String
 formatKeyword str = if ((all isAlpha str) && (all isAlpha str))
                     then toUpperWord $ removeDuplicated $ (convertJI str)
@@ -9,17 +40,8 @@ formatKeyword str = if ((all isAlpha str) && (all isAlpha str))
                     where
                         removeDuplicated [] = []
                         removeDuplicated (x:xs) = x : removeDuplicated (filter (/= x) xs)
-                        convertJI str = map (\x -> if x == 'J' then 'I' else x) str
-                        toUpperWord str = map toUpper str
-
-createDiagramList :: String -> String
-createDiagramList keyword = keyword ++ abcWithoutKeys (Set.fromList keyword) 
-                            where
-                            abcWithoutKeys keys = filter (\x -> x/='J' && Set.notMember x keys) ['A'..'Z']
-
-createDiagram :: String -> [String] 
-createDiagram unformatedKey = toMatrix(createDiagramList $ formatKeyword unformatedKey)
-                              where
-                              chunk _ [] = []
-                              chunk n xs = take n xs : chunk n (drop n xs)
-                              toMatrix = chunk 5
+                        
+formatText :: String -> String
+formatText str = if ((all isAlpha str) && (all isAlpha str))
+                    then toUpperWord $ (convertJI str)
+                    else error "Please insert alpha keyword"
