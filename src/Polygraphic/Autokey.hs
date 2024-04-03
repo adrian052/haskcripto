e3d9413f30350e766e07b7db505cfac20c2d0e2a
adrian052 <a_drian1@hotmail.es>
2024-04-03 14:36:57 -0600

    Added autokey cipher

--- src/Polygraphic/Autokey.hs
+++ src/Polygraphic/Autokey.hs
@@ -1,23 +1,26 @@
+module Substitution.RailFence (
+    encryptAutokey,
+    decryptAutokey
+) where
 
 import Data.Char
 
-encryptAutokey :: String -> Int -> String
-encryptAutokey text key = map (\sum -> chr ((mod sum 26)+ord 'A')) (zipWith (+) pvalues keystream') 
-            where 
-            pvalues =  pValues $ formatText text
-            keystream' = keystream key pvalues
+charToNum :: Char -> Int
+charToNum c = ord c - ord 'A'
 
---HELPER FUNCTIONS 
-
-keystream :: Int -> [Int] -> [Int]
-keystream head pValues = head: pValues
-
-pValues :: String -> [Int]
-pValues = map (\x -> ord x - ord 'A')
+numToChar :: Int -> Char
+numToChar n = chr (n `mod` 26 + ord 'A')
 
 formatText :: String -> String
 formatText str = if ((all isAlpha str) && (all isAlpha str))
                     then toUpperWord str
                     else error "Please insert alpha keyword"
-                    where 
-                    toUpperWord str = map toUpper strformatText $
+
+toUpperWord :: String -> String
+toUpperWord str = map toUpper str
+
+encryptAutokey :: String -> String -> String
+encryptAutokey key plaintext = map numToChar $ zipWith (\p k -> (charToNum p + charToNum k) `mod` 26) (formatText plaintext) (cycle key)
+
+decryptAutokey :: String -> String -> String
+decryptAutokey key ciphertext = map numToChar $ zipWith (\c k -> (charToNum c - charToNum k) `mod` 26) ciphertext (cycle key)
