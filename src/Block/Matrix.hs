7af892aeecdc50c2d7f2ae95e320a408e1dc8b48
adrian052 <a_drian1@hotmail.es>
2024-04-20 09:53:56 -0600

    Added apply permutation function

--- src/Block/Matrix.hs
+++ src/Block/Matrix.hs
@@ -26,11 +26,20 @@ addLastColumn col matrix = zipWith (\row c -> row ++ [c]) matrix col
 getColumn :: Int -> Matrix a -> [a]
 getColumn colIndex = map (!! colIndex)
 
-xorBitMatrix :: BitMatrix -> BitMatrix -> BitMatrix
-xorBitMatrix = zipWith $ zipWith xor
-
 group :: Int -> [a] -> Matrix a
 group _ [] = []
 group n l
   | n > 0 = (take n l) : (group n (drop n l))
   | otherwise = error "Negative or zero n"
+
+applyPermutation :: [[(Int, Int)]] -> Matrix a -> Matrix a
+applyPermutation permutationMatrix matrix =
+  let numRows = length matrix
+      numCols = length (head matrix)
+      newPosition (r, c) = (permutationMatrix !! r !! c)
+      newElement (r, c) = matrix !! r !! c
+      result = [[newElement (newPosition (r, c)) | c <- [0 .. numCols -1]] | r <- [0 .. numRows -1]]
+   in result
+
+xorBitMatrix :: BitMatrix -> BitMatrix -> BitMatrix
+xorBitMatrix = zipWith $ zipWith xor
